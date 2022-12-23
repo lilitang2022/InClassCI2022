1dc5ccff2daeca9c87115713ae3e28a64a829c34
lilitang2022 <112470886+lilitang2022@users.noreply.github.com>
2022-12-22 22:57:14 -0800

    Update Test-pkg1.py

--- Test-pkg1.py
+++ Test-pkg1.py
@@ -80,3 +80,12 @@ class Test_check(unittest.TestCase):
         self.assertFalse(right2.check_IsNull())          
         
 unittest.main()
+
+def my_suite():
+    suite = unittest.TestSuite()
+    result = unittest.TestResult()
+    suite.addTest(unittest.makeSuite(Test_output))
+    suite.addTest(unittest.makeSuite(Test_check))
+    runner = unittest.TextTestRunner()
+    print(runner.run(suite))
+my_suite()
