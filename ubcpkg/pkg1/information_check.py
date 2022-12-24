f7de5ecd66e7c5e97902d9f2372cdffcde06edf8
lilitang2022 <112470886+lilitang2022@users.noreply.github.com>
2022-12-23 17:22:20 -0800

    Add files via upload

--- ubcpkg/pkg1/information_check.py
+++ ubcpkg/pkg1/information_check.py
@@ -1,14 +1,14 @@
 class Info_Check():
     
     def __init__(self,name,iid):
-        self.list = ["12345","54321"]
+        self.list = [12345,54321]
         self.name = name
         self.iid = iid
     
     def check_inlist(self):
-        if self.iid.lower() in self.list:
-            return True
-        else:
+         if self.iid in self.list:
+            return True 
+         else:
             return False
         
     def check_IsNull(self):
